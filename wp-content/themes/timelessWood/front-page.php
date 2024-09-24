29bcbf8327cd18c5fcd7021cbf4cb7ea376d6a40
yannick <yannick.van.eck@gmail.com>
2024-09-24 13:57:47 +0200

    uploaded media to local site + hero section

--- wp-content/themes/timelessWood/front-page.php
+++ wp-content/themes/timelessWood/front-page.php
@@ -1,8 +1,16 @@
 <?php
     get_header();
 ?>
-
-    <h1>Masters in outdoor wood projects</h1>
+<div class="hero-section">
+    <div class="hero-title-container">
+        <h1 id="hero-title" >Masters in outdoor wood projects</h1>
+        <div class="hero-button-container">
+            <a id="hero-button-L" class="hero-button" href="#">Ontdek ons aanbod</a>
+            <a id="hero-button-R" class="hero-button" href="#">Contacteer ons</a>
+        </div>
+    </div>
+</div>
+    
 
 <?php
 get_footer();
