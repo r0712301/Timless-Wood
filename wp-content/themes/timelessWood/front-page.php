5fd9be684f362278fefe781c4f72b6fcc7639aa5
yannick <yannick.van.eck@gmail.com>
2024-09-25 16:21:57 +0200

    scrolling cards + font awesome added

--- wp-content/themes/timelessWood/front-page.php
+++ wp-content/themes/timelessWood/front-page.php
@@ -10,6 +10,54 @@
         </div>
     </div>
 </div>
+<hr id="hero-line-vert">
+
+<div class="slogan-section">
+    <div class="slogan-image-container">
+        <div id="gray-box"></div>
+        <img class="slogan-image" src="http://sollicitatiewebit.local/wp-content/uploads/2024/09/passie.png" alt="houtbewerking">
+    </div>
+    <div class="slogan-text-container">
+        <h2 class="slogan-title">“Bouwen met hout, een pure passie”</h2>
+        <p class="slogan-text">Timeless Wood heeft een team van ervaren mensen achter zich staan, dat met passie en vakmanschap alle houtconstructies bouwt volledig naar de wensen van de klant. Streven naar volledige klantentevredenheid is onze grootste drijfveer.</p>
+        <a href="#" class="slogan-button">Meer over ons</a>
+    </div>
+</div>
+
+<div class="aanbod-container">
+    <div>
+        <h2 class="aanbod-title">Aanbod</h2>
+        <p class="aanbod-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent efficitur sollicitudin varius. Vestibulum quis erat aliquet, fermentum tortor at, iaculis lacus.</p>
+    </div>
+    <div class="aanbod-card-carousel">
+        <ul class="cards">
+            <li class="card">
+                <img src="http://sollicitatiewebit.local/wp-content/uploads/2024/09/4fb67c488a60e987777515c229f005c6.png" alt="" class="card-image">
+                <h3 class="card-title">Bijgebouwen</h3>
+                <p class="card-content">Outdoor living in harmonie met tuin en omgeving</p>
+                <a href="#"><i class="fa-solid fa-plus fa-2xl" style="color: #000;"></i></a>
+            </li>
+            <li class="card">
+                <img src="http://sollicitatiewebit.local/wp-content/uploads/2024/09/0324383de8bd8dc28d21e60df456a3ab.png" alt="" class="card-image">
+                <h3 class="card-title">Terrasbouw</h3>
+                <p class="card-content">Genieten in eigen tuin</p>
+                <a href="#"><i class="fa-solid fa-plus fa-2xl" style="color: #000;"></i></a>
+            </li>
+            <li class="card">
+                <img src="http://sollicitatiewebit.local/wp-content/uploads/2024/09/8027b691796fe06c7e901dea10fdf892.png" alt="" class="card-image">
+                <h3 class="card-title">Poorten</h3>
+                <p class="card-content">Stijlvolle inkom als eerste indruk</p>
+                <a href="#"><i class="fa-solid fa-plus fa-2xl" style="color: #000;"></i></a>
+            </li>
+            <li class="card">
+                <img src="http://sollicitatiewebit.local/wp-content/uploads/2024/09/45e3bec10d23a58255420dbf3c412f1b.png" alt="" class="card-image">
+                <h3 class="card-title">Project & interior</h3>
+                <p class="card-content">Massief houten creaties</p>
+                <a href="#"><i class="fa-solid fa-plus fa-2xl" style="color: #000;"></i></a>
+            </li>
+        </ul>
+    </div>
+</div>
     
 
 <?php
--- wp-content/themes/timelessWood/functions.php
+++ wp-content/themes/timelessWood/functions.php
@@ -5,4 +5,9 @@
     }
 
     add_action('wp_enqueue_scripts', 'timelesswood_register_styles');
+
+    function timelesswood_register_scripts(){
+        wp_enqueue_script('fontawesome', 'https://kit.fontawesome.com/3d2f8be472.js', array(), '1.0', 'all');
+    }
+    add_action('wp_enqueue_scripts', 'timelesswood_register_scripts');
 ?>
