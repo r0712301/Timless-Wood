0244e57e5ecf50d6f5dadae3cac7e155fe5ee274
yannick <yannick.van.eck@gmail.com>
2024-09-25 22:25:43 +0200

    footer + homepage runthrough

--- wp-content/themes/timelessWood/footer.php
+++ wp-content/themes/timelessWood/footer.php
@@ -1,5 +1,49 @@
+<hr class="footer-seperator">
+<footer>
+    <img src="wp-content\themes\timelessWood\assets\images\logo.png" alt="logo" class="logo">
+    <div class="footer-contact-container">
+        <h5 class="footer-contact-title">CONTACT</h5>
+        <p class="footer-contact-text">Brasschaatsteenweg 104<br> 2920 Kalmthout</p>
+        <p><br></p>
+        <p>
+            <a href="tel:+3236516200" class="footer-contact-text">+32 (0)3 651 62 00</a>
+        </p>
+        <p>
+            <a href="mailto:info@timelesswood.be" class="footer-contact-text">info@timelesswood.be</a>
+        </p>
+    </div>
+    <div class="footer-aanbod-container">
+        <h5 class="footer-aanbod-title">AANBOD</h5>
+        <p><a href="#" class="footer-link">Bijgebouwen</a></p>
+        <p><a href="#" class="footer-link">Terrasbouw</a></p>
+        <p><a href="#" class="footer-link">Poorten</a></p>
+        <p><a href="#" class="footer-link">Project & Interior</a></p>
+    </div>
+    <div class="footer-snellelinks-container">
+        <h5 class="footer-snellelinks-title">SNELLE LINKS</h5>
+        <p><a href="#" class="footer-link">Over ons</a></p>
+        <p><a href="#" class="footer-link">Realisaties</a></p>
+        <p><a href="#" class="footer-link">Nieuws</a></p>
+        <p><a href="#" class="footer-link">Contact</a></p>
+    </div>
+    <div class="footer-socials-container">
+        <a href="#"><i id="fb" class="fa-brands fa-facebook-f fa-2xl footer-social-link" style="color: #3d3f32;"></i></a>
+        <a href="#"><i id="insta" class="fa-brands fa-instagram fa-2xl footer-social-link" style="color: #3d3f32;"></i></a>
+    </div>
+</footer>
+
+<hr id="under-footer" class="footer-seperator">
+<div class="copy-container">
+    <div class="copy">
+        <p>© Timeless Wood 2023 – Webdesign Webit</p>
+    </div>
+    <div class="privacy">
+        <p>Privacy | Cookiebeleid</p>
+    </div>
+</div>
 <!-- Main container end -->
 </div>
-<?php wp_footer();?>
+<?php wp_footer(); ?>
 </body>
+
 </html>
