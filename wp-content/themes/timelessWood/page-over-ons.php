2d4fbbefd6b1b476ec73465f16f5b74e237bded6
yannick <yannick.van.eck@gmail.com>
2024-09-26 11:47:50 +0200

    header, footer update + page has header & footer + over ons page dev until timline setup

---
+++ wp-content/themes/timelessWood/page-over-ons.php
@@ -0,0 +1,69 @@
+<?php
+get_header();
+?>
+<div class="over-ons-hero-container">
+    <div class="over-ons-hero-overlay-container">
+        <h1 class="over-ons-hero-title">Over ons</h1>
+        <p class="over-ons-hero-text">Timeless Wood heeft een team van ervaren mensen achter zich staan, dat met passie en vakmanschap alle houtconstructies bouwt volledig naar de wensen van de klant. Streven naar volledige klantentevredenheid is onze grootste drijfveer.</p>
+        <a href="#" class="over-ons-hero-btn">Ontdek ons team</a>
+    </div>
+</div>
+<hr id="over-ons-hero-line-vert">
+
+<div class="over-ons-waarden-container">
+    <div id="over-ons-waarde-1" class="over-ons-waarde-container">
+        <img src="http://sollicitatiewebit.local/wp-content/uploads/2024/09/Layer_2.png" alt="" class="over-ons-waarde-icon">
+        <h3 class="over-ons-waarde-title">Bouwen met hout<br>Een pure passie</h3>
+        <p class="over-ons-waarde-text">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.</p>
+    </div>
+    <div id="over-ons-waarde-2" class="over-ons-waarde-container">
+        <img src="http://sollicitatiewebit.local/wp-content/uploads/2024/09/Layer_3.png" alt="" class="over-ons-waarde-icon">
+        <h3 class="over-ons-waarde-title">Vakmanschap</h3>
+        <p class="over-ons-waarde-text">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.</p>
+    </div>
+    <div id="over-ons-waarde-3" class="over-ons-waarde-container">
+        <img src="http://sollicitatiewebit.local/wp-content/uploads/2024/09/Layer_4.png" alt="" class="over-ons-waarde-icon">
+        <h3 class="over-ons-waarde-title">Duurzaamheid</h3>
+        <p class="over-ons-waarde-text">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.</p>
+    </div>
+    <div id="over-ons-waarde-4" class="over-ons-waarde-container">
+        <img src="http://sollicitatiewebit.local/wp-content/uploads/2024/09/Layer_5.png" alt="" class="over-ons-waarde-icon">
+        <h3 class="over-ons-waarde-title">Ruim aanbod</h3>
+        <p class="over-ons-waarde-text">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.</p>
+    </div>
+</div>
+
+<div class="over-ons-team-container">
+    <div class="over-ons-team-title-container">
+        <h2 class="over-ons-team-title">Ontmoet het team</h2>
+    </div>
+    <div class="over-ons-team-text-foto-container">
+        <div class="over-ons-team-text-container">
+            <p class="over-ons-team-text">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, <strong>sed diam voluptua</strong>. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. <br><br> Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, <u>sed diam voluptua.</u></p>
+            <a href="#" class="over-ons-team-btn">Lorem ipsum</a>
+        </div>
+        <img src="http://sollicitatiewebit.local/wp-content/uploads/2024/09/annie-spratt-unsplash.png" alt="" class="over-ons-team-foto">
+    </div>
+</div>
+
+<div class="over-ons-werken-container">
+    <h2 class="over-ons-werken-title">Hoe werken wij</h2>
+    <p class="over-ons-werken-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent efficitur sollicitudin varius. Vestibulum quis erat aliquet, fermentum tortor at, iaculis lacus.</p>
+    <div class="over-ons-werken-timeline">
+        <div class="over-ons-werken-timeline-container right">
+            <div class="over-ons-werken-timeline-content">
+                <h2>2017</h2>
+                <p>Lorem ipsum..</p>
+            </div>
+        </div>
+        <div class="over-ons-werken-timeline-container left">
+            <div class="over-ons-werken-timeline-content">
+                <h2>2016</h2>
+                <p>Lorem ipsum..</p>
+            </div>
+        </div>
+    </div>
+</div>
+<?php
+get_footer();
+?>
