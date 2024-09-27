caeecd869ad5ccaaa25a297132eaff3ed000c213
yannick <yannick.van.eck@gmail.com>
2024-09-27 09:56:49 +0200

    small rework

--- wp-content/themes/timelessWood/page-realisaties.php
+++ wp-content/themes/timelessWood/page-realisaties.php
@@ -7,17 +7,32 @@ get_header();
     <h1 class="realisatie-page-title">Realisaties</h1>
     <p class="realisatie-page-text">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit.</p>
     <div class="realisatie-page-cards-conatainer">
-        <div class="realisatie-page-card">
+        <div class="realisatie-page-card" style="background-image: linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2)),url('http://sollicitatiewebit.local/wp-content/uploads/2024/09/1f5ce17b0ba186e9c1d0604371a4702c.png');">
             <div>
-                <h3 class="realisatie-card-title">NJOYIN THE POOL - ZOERSEL</h3>
-                <a href="" class="realisatie-card-btn">Terrasbouw</a>
+                <h3 class="realisatie-page-card-title">NJOYIN THE POOL - ZOERSEL</h3>
+                <div class="realisatie-tag-container">
+                    <a href="" class="realisatie-page-card-btn">Terrasbouw</a>
+                </div>
+
             </div>
 
         </div>
-        <div class="realisatie-page-card">
+        <div class="realisatie-page-card" style="background-image: linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2)),url('http://sollicitatiewebit.local/wp-content/uploads/2024/09/1f5ce17b0ba186e9c1d0604371a4702c.png');">
             <div>
                 <h3 class="realisatie-page-card-title">ENJOY YOUR STAY!</h3>
-                <a href="" class="realisatie-page-card-btn">Bijgebouwen hedendaags</a>
+                <div class="realisatie-tag-container">
+                    <a href="" class="realisatie-page-card-btn">Bijgebouwen hedendaags</a>
+                </div>
+
+            </div>
+
+        </div>
+        <div class="realisatie-page-card" style="background-image: linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2)),url('http://sollicitatiewebit.local/wp-content/uploads/2024/09/1f5ce17b0ba186e9c1d0604371a4702c.png');">
+            <div>
+                <h3 class="realisatie-page-card-title">ENJOY YOUR STAY!</h3>
+                <div class="realisatie-tag-container">
+                    <a href="" class="realisatie-page-card-btn">Bijgebouwen Landelijk</a><a href="" class="realisatie-page-card-btn">Poorten</a>
+                </div>
             </div>
 
         </div>
